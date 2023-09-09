afe9b229be6f4a30d10fdc7cd19e06650ef15fd8
Jiale <cjl601523@gmail.com>
2023-09-09 13:31:04 +0800

    feat: 新增后端框架及登录接口

---
+++ platform/src/main/java/com/seu/platform/common/model/Result.java
@@ -0,0 +1,37 @@
+package com.seu.platform.common.model;
+
+import com.seu.platform.common.enums.ResultCode;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @author chenjiale
+ * @version 1.0
+ * @date 2023-08-14 22:49
+ */
+@Data
+@AllArgsConstructor
+public class Result<T> implements Serializable {
+    private int code;
+    private String type;
+    private String message;
+    private T result;
+
+    public Result(ResultCode code, String message, T result) {
+        this.code = code.code;
+        this.type = code.type;
+        this.message = message;
+        this.result = result;
+    }
+
+    public static <T> Result<T> success(T data) {
+        return new Result<>(ResultCode.SUCCESS, "ok", data);
+    }
+
+    public static <T> Result<T> fail(String message) {
+        return new Result<>(ResultCode.FAIL, message, null);
+    }
+
+}
