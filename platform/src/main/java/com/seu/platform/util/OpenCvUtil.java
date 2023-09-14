34a7f9b39338a080632b2d589efdb367ac08830d
CJL6015 <1253368419@qq.com>
2023-09-14 21:00:24 +0800

    feat: 新增yolov工具

--- platform/src/main/java/com/seu/platform/util/OpenCvUtil.java
+++ platform/src/main/java/com/seu/platform/util/OpenCvUtil.java
@@ -1,15 +1,20 @@
 package com.seu.platform.util;
 
+import ai.onnxruntime.OnnxTensor;
+import ai.onnxruntime.OrtEnvironment;
+import ai.onnxruntime.OrtException;
+import ai.onnxruntime.OrtSession;
 import lombok.experimental.UtilityClass;
-import org.bytedeco.opencv.global.opencv_core;
-import org.bytedeco.opencv.global.opencv_dnn;
-import org.bytedeco.opencv.global.opencv_imgcodecs;
-import org.bytedeco.opencv.global.opencv_imgproc;
-import org.bytedeco.opencv.opencv_core.Mat;
-import org.bytedeco.opencv.opencv_core.Size;
-import org.bytedeco.opencv.opencv_dnn.Net;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
 
-import java.io.File;
+import java.io.IOException;
+import java.nio.FloatBuffer;
+import java.util.HashMap;
 
 /**
  * @author chenjiale
@@ -18,23 +23,60 @@ import java.io.File;
  */
 @UtilityClass
 public class OpenCvUtil {
-    public static void main(String[] args) {
-
-        String modelPath = "/Users/cjl1996/Downloads/yolov5n-7-k5.onnx";
+    public static void main(String[] args) throws OrtException, IOException {
+        nu.pattern.OpenCV.loadLocally();
+        String modelPath = "/Users/cjl1996/Downloads/yolov5s1.onnx";
         String imagePath = "/Users/cjl1996/Downloads/673999.png";
-        File file1 = new File(imagePath);
-        System.out.println(file1.exists());
-        Mat image = opencv_imgcodecs.imread(imagePath);
-        File file = new File(modelPath);
-        System.out.println(file.exists());
-        Net net = opencv_dnn.readNetFromONNX(modelPath);
-        Size size = new Size(660, 660);
-        opencv_imgproc.resize(image, image, size);
-        Mat blob = opencv_dnn.blobFromImage(image);
-        net.setInput(image);
-        Mat detections = net.forward();
-        int numDetections = detections.rows();
-        opencv_core.print(detections);
+        String outPath = "/Users/cjl1996/Downloads/yolov.png";
+        OrtEnvironment environment = OrtEnvironment.getEnvironment();
+        OrtSession.SessionOptions sessionOptions = new OrtSession.SessionOptions();
+        OrtSession session = environment.createSession(modelPath, sessionOptions);
+        Mat image = Imgcodecs.imread(imagePath);
+        Mat outImage = Imgcodecs.imread(imagePath);
+        VideoLetterbox letterbox = new VideoLetterbox();
+        image = letterbox.letterbox(image);
+        Imgproc.cvtColor(image, image, Imgproc.COLOR_BGR2RGB);
+        image.convertTo(image, CvType.CV_32FC1, 1. / 255);
+        float[] whc = new float[3 * 640 * 640];
+        image.get(0, 0, whc);
+        float[] chw = ImageUtil.whc2cwh(whc);
+
+
+        FloatBuffer inputBuffer = FloatBuffer.wrap(chw);
+        OnnxTensor tensor = OnnxTensor.createTensor(environment, inputBuffer, new long[]{1, 3, 640, 640});
+
+
+        HashMap<String, OnnxTensor> stringOnnxTensorHashMap = new HashMap<>();
+        stringOnnxTensorHashMap.put(session.getInputInfo().keySet().iterator().next(), tensor);
+
+        // 运行推理
+        // 模型推理本质是多维矩阵运算，而GPU是专门用于矩阵运算，占用率低，如果使用cpu也可以运行，可能占用率100%属于正常现象，不必纠结。
+        OrtSession.Result output = session.run(stringOnnxTensorHashMap);
+
+        // 得到结果,缓存结果
+        float[][] outputData = ((float[][][]) output.get(0).getValue())[0];
+        for (float[] x : outputData) {
+            if (x[6] < 0.25) {
+                continue;
+            }
+            ODResult odResult = new ODResult(x);
+            // 画框
+            Point topLeft = new Point((odResult.getX0() - letterbox.getDw()) / letterbox.getRatio(), (odResult.getY0() - letterbox.getDh()) / letterbox.getRatio());
+            Point bottomRight = new Point((odResult.getX1() - letterbox.getDw()) / letterbox.getRatio(), (odResult.getY1() - letterbox.getDh()) / letterbox.getRatio());
+            Scalar color = new Scalar(255, 0, 0);
+
+            Imgproc.rectangle(outImage, topLeft, bottomRight, color, 5);
+            // 框上写文字
+            String boxName = "person";
+            Point boxNameLoc = new Point((odResult.getX0() - letterbox.getDw()) / letterbox.getRatio(), (odResult.getY0() - letterbox.getDh()) / letterbox.getRatio() - 3);
+
+            // 也可以二次往视频画面上叠加其他文字或者数据，比如物联网设备数据等等
+            Imgproc.putText(outImage, boxName, boxNameLoc, Imgproc.FONT_HERSHEY_SIMPLEX, 10, color, 20);
+            System.out.println(odResult + "   " + boxName);
+
+        }
+        Imgcodecs.imwrite(outPath, outImage);
         System.out.println(11);
     }
+
 }
---
+++ platform/src/main/java/com/seu/platform/util/VideoLetterbox.java
@@ -0,0 +1,85 @@
+package com.seu.platform.util;
+
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+
+class VideoLetterbox {
+
+    private Size newShape = new Size(640, 640);
+    private final double[] color = new double[]{114,114,114};
+    private final Boolean auto = false;
+    private final Boolean scaleUp = true;
+    private Integer stride = 32;
+
+    private double ratio;
+    private double dw;
+    private double dh;
+
+    public double getRatio() {
+        return ratio;
+    }
+
+    public double getDw() {
+        return dw;
+    }
+
+    public Integer getWidth() {
+        return (int) this.newShape.width;
+    }
+
+    public Integer getHeight() {
+        return (int) this.newShape.height;
+    }
+
+    public double getDh() {
+        return dh;
+    }
+
+
+     Size newUnpad;
+     int top;
+     int bottom;
+     int left;
+     int right;
+
+    public Mat letterbox(Mat im) { // 调整图像大小和填充图像，使满足步长约束，并记录参数
+
+        // Compute padding
+        if(newUnpad == null){
+            int[] shape = {im.rows(), im.cols()}; // 当前形状 [height, width]
+            // Scale ratio (new / old)
+            double r = Math.min(this.newShape.height / shape[0], this.newShape.width / shape[1]);
+            if (!this.scaleUp) { // 仅缩小，不扩大（一且为了mAP）
+                r = Math.min(r, 1.0);
+            }
+            this.ratio = r;
+            newUnpad = new Size(Math.round(shape[1] * r), Math.round(shape[0] * r));
+            double dw = this.newShape.width - newUnpad.width, dh = this.newShape.height - newUnpad.height; // wh 填充
+            if (this.auto) { // 最小矩形
+                dw = dw % this.stride;
+                dh = dh % this.stride;
+            }
+            dw /= 2; // 填充的时候两边都填充一半，使图像居于中心
+            dh /= 2;
+            if (shape[1] != newUnpad.width || shape[0] != newUnpad.height) { // resize
+                Imgproc.resize(im, im, newUnpad, 0, 0, Imgproc.INTER_LINEAR);
+            }
+             top = (int) Math.round(dh - 0.1);
+             bottom = (int) Math.round(dh + 0.1);
+             left = (int) Math.round(dw - 0.1);
+             right = (int) Math.round(dw + 0.1);
+            // 将图像填充为正方形
+            Core.copyMakeBorder(im, im, top, bottom, left, right, Core.BORDER_CONSTANT, new Scalar(this.color));
+
+            this.dh = dh;
+            this.dw = dw;
+        }else{
+            Imgproc.resize(im, im, newUnpad, 0, 0, Imgproc.INTER_LINEAR);
+            Core.copyMakeBorder(im, im, top, bottom, left, right, Core.BORDER_CONSTANT, new Scalar(this.color));
+        }
+        return im;
+    }
+}
